47c3a46391ca119ddc38b3741bc2c572484325f3
User <User@ithub.tulskaya>
2023-03-15 19:02:33 +0300

    Initial

---
+++ Birj/Assets/_source/Scripts/MonaeySO.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonaeySO : ScriptableObject
+{
+    [SerializeField] string moneyName;
+    [SerializeField] float minPricae;
+    [SerializeField] float maxPricae;
+    [SerializeField] Sprite image;
+    [SerializeField] float changeKof;
+    [SerializeField] float renewTime;
+
+    
+}
---
+++ Birj/Assets/_source/Scripts/Wallet.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Wallet
+{
+    private float _boughtKrokrns = 0;
+    private float _boughtDobbners = 0;
+    private float _boughtUkhts = 0;
+    [SerializeField] private float _currentSchirshi;
+    [SerializeField] TMP_Text wallet;
+    [SerializeField] TMP_Text Krokrns;
+    [SerializeField] TMP_Text Dobbners;
+    [SerializeField] TMP_Text Ukhts;
+
+    
+    
+}
